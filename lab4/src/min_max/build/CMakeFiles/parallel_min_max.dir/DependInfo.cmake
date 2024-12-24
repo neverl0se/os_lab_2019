81fb7d676749c10b50940a0c669d6d5e64dc3cfc
Anatoly Surogin <63653141+neverl0se@users.noreply.github.com>
2024-12-24 13:14:21 +0000

    src

---
+++ lab4/src/min_max/build/CMakeFiles/parallel_min_max.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/Desktop/os_lab_2019/lab4/src/min_max/find_min_max.c" "CMakeFiles/parallel_min_max.dir/find_min_max.c.o" "gcc" "CMakeFiles/parallel_min_max.dir/find_min_max.c.o.d"
+  "/home/user/Desktop/os_lab_2019/lab4/src/min_max/parallel_min_max.c" "CMakeFiles/parallel_min_max.dir/parallel_min_max.c.o" "gcc" "CMakeFiles/parallel_min_max.dir/parallel_min_max.c.o.d"
+  "/home/user/Desktop/os_lab_2019/lab4/src/min_max/utils.c" "CMakeFiles/parallel_min_max.dir/utils.c.o" "gcc" "CMakeFiles/parallel_min_max.dir/utils.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
